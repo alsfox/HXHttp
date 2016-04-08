d23a7562657e341e912ee8504dfc872c4ec80264
Yakuma <374814789@qq.com>
2016-04-08 10:27:55 +0800

    初始化

---
+++ HXHttpSign.framework/Headers/HXHttpSign.h
@@ -0,0 +1,21 @@
+//
+//  HXHttpSign.h
+//  HXHttpSign
+//
+//  Created by 张亚举 on 16/4/7.
+//  Copyright © 2016年 张亚举. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "NSDict+Extension.h"
+
+//! Project version number for HXHttpSign.
+FOUNDATION_EXPORT double HXHttpSignVersionNumber;
+
+//! Project version string for HXHttpSign.
+FOUNDATION_EXPORT const unsigned char HXHttpSignVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <HXHttpSign/PublicHeader.h>
+
+
---
+++ HXHttpSign.framework/Headers/NSDict+Extension.h
@@ -0,0 +1,16 @@
+//
+//  NSDict+Extension.h
+//  AXBaseMall
+//
+//  Created by King on 15/9/4.
+//  Copyright (c) 2015年 King. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NSDictionary (Extension1)
+
+/** 得到sign 字符验签 */
+- (NSDictionary *) lz_sign;
+
+@end
